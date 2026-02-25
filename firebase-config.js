fd4914fa3f0f13e2df6ae86a76b6fb4c7e015ff2
makeprofit366-art <makeprofit366@gmail.com>
2026-02-25 23:16:33 +0530

    Refactor Firebase initialization and add auth functions

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,42 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase is loaded via CDN scripts in HTML files
+// DO NOT use import statements here
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBi4Udgz97OJo1sei1pAmCJ9oiBT4BSGMw",
   authDomain: "btech-vault.firebaseapp.com",
   projectId: "btech-vault",
   storageBucket: "btech-vault.firebasestorage.app",
   messagingSenderId: "8508597620",
-  appId: "1:8508597620:web:f36418f8c702e8a21c3821",
-  measurementId: "G-X0TY8S1TPB"
+  appId: "1:8508597620:web:f36418f8c702e8a21c3821"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+async function signInWithGoogle() {
+  try {
+    const result = await auth.signInWithPopup(googleProvider);
+    const user = result.user;
+    await db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL,
+      joinedAt: firebase.firestore.FieldValue.serverTimestamp()
+    }, { merge: true });
+    return user;
+  } catch (error) {
+    console.error("Login error:", error);
+    throw error;
+  }
+}
+
+async function signOut() {
+  await auth.signOut();
+  window.location.href = 'index.html';
+}
